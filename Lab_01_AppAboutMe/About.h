36ba6b1763176bcd6236f579ad8ce2e6d7d56475
Milja <mmcvirtanen@gmail.com>
2018-01-18 20:25:55 +0100

    Added About and Interest views.

---
+++ Lab_01_AppAboutMe/About.h
@@ -0,0 +1,13 @@
+//
+//  About.h
+//  Lab_01_AppAboutMe
+//
+//  Created by Milja Virtanen on 2018-01-18.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface About : UIViewController
+
+@end
---
+++ Lab_01_AppAboutMe/Interests.h
@@ -0,0 +1,13 @@
+//
+//  Interests.h
+//  Lab_01_AppAboutMe
+//
+//  Created by Milja Virtanen on 2018-01-18.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Interests : UIViewController
+
+@end
