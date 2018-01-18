9ca3827cb955639aa18053238afbe0edef172c8b
Milja <mmcvirtanen@gmail.com>
2018-01-18 19:08:23 +0100

    Added flag to ViewController.

--- Lab_01_AppAboutMe/ViewController.m
+++ Lab_01_AppAboutMe/ViewController.m
@@ -7,32 +7,49 @@
 //
 
 #import "ViewController.h"
+#import "CustomColorsViewController.h"
+
+
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *navigation;
+@property (weak, nonatomic) IBOutlet UILabel *game;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *about;
+@property (weak, nonatomic) IBOutlet UIButton *interests;
+@property (weak, nonatomic) IBOutlet UIButton *whyITHS;
+@property (weak, nonatomic) IBOutlet UIButton *numberGame;
+@property (weak, nonatomic) IBOutlet UIButton *colors;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    BOOL firstLoad;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"FIRST LOAD");
+    firstLoad = YES;
 }
 
-//GET SLIDER VALUE
-
-//METHOD: PLAY A ROUND
-- (void)guessNumber {
+-(void)viewWillAppear:(BOOL)animated {
     
-    //Computer generates random number, compared to slider value
-    
-
-    //Outcomes: Too high, too low, correct
-
+    [super viewWillAppear:animated];
+    if (!firstLoad)
+        [self loadCustomColors];
+    firstLoad = NO;
+    NSLog(@"SECOND LOAD");
 }
 
-
+-(void)loadCustomColors {
+    self.view.backgroundColor = [CustomColorsViewController bgColor];
+    self.navigation.textColor = [CustomColorsViewController labelColor];
+    self.game.textColor = [CustomColorsViewController labelColor];
+    self.imageView.backgroundColor = [CustomColorsViewController bgColor];
+    self.about.tintColor = [CustomColorsViewController buttonColor];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
