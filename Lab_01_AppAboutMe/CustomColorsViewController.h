1ad5995d4c04beb610f2c1c1571ec26401479b94
Milja <mmcvirtanen@gmail.com>
2018-01-18 19:04:14 +0100

    Fixed code in CustomColorsViewController.

---
+++ Lab_01_AppAboutMe/CustomColorsViewController.h
@@ -0,0 +1,25 @@
+//
+//  CustomColorsViewController.h
+//  Lab_01_AppAboutMe
+//
+//  Created by Milja Virtanen on 2018-01-17.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomColorsViewController : UIViewController
+
+//TEST
+@property (nonatomic, strong) UIColor *bgColor;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong) UIColor *buttonColor;
+@property (nonatomic, strong) UIColor *labelColor;
+
++(UIColor*)bgColor;
++(UIColor*)textColor;
++(UIColor*)buttonColor;
++(UIColor*)textViewColor;
++(UIColor*)labelColor;
+
+@end
---
+++ Lab_01_AppAboutMe/CustomColorsViewController.m
@@ -0,0 +1,125 @@
+//
+//  CustomColorsViewController.m
+//  Lab_01_AppAboutMe
+//
+//  Created by Milja Virtanen on 2018-01-17.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import "CustomColorsViewController.h"
+
+//Static color values accessable from other classes
+UIColor *bgColor;
+UIColor *textColor;
+UIColor *buttonColor;
+UIColor *textViewColor;
+UIColor *labelColor;
+
+@interface CustomColorsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+//Color values
+/*
+@property (strong, nonatomic) UIColor *bgColor;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong) UIColor *buttonColor;
+@property (nonatomic, strong) UIColor *labelColor;
+*/
+@end
+
+@implementation CustomColorsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
++ (UIColor*)bgColor {
+    return bgColor;
+}
+
++ (UIColor*)textColor {
+    return textColor;
+}
+
++ (UIColor*)buttonColor {
+    return buttonColor;
+}
+
++ (UIColor*)textViewColor {
+    return textViewColor;
+}
+
++ (UIColor*)labelColor {
+    return labelColor;
+}
+
+- (IBAction)monochromeColorsButton:(id)sender {
+    [self setMonochromeColor];
+    
+    self.view.backgroundColor = bgColor;
+    self.button1.tintColor = self.buttonColor;
+    self.button2.tintColor = self.buttonColor;
+    self.textView.backgroundColor = self.bgColor;
+    self.label.textColor = self.labelColor;
+}
+
+- (IBAction)colorfulColorsButton:(id)sender {
+    [self setColorfulColors];
+    
+    self.view.backgroundColor = bgColor;
+    self.button1.tintColor = buttonColor;
+    self.button2.tintColor = buttonColor;
+    self.textView.backgroundColor = bgColor;
+    self.label.textColor = labelColor;
+}
+
+//Sets variable values to colorful scheme
+-(void)setColorfulColors {
+    bgColor = [UIColor colorWithRed:0.96 green:0.83 blue:0.83 alpha:1.0];
+    textColor = [UIColor blackColor];
+    buttonColor = [UIColor lightGrayColor];
+    textViewColor = bgColor;
+    labelColor = [UIColor colorWithRed:0.76 green:0.29 blue:0.30 alpha:1.0];
+    
+    //self.bgColor = [UIColor colorWithRed:0.96 green:0.83 blue:0.83 alpha:1.0];
+    //self.textColor = [UIColor blackColor];
+    //self.buttonColor = [UIColor lightGrayColor];
+    //self.labelColor = [UIColor colorWithRed:0.76 green:0.29 blue:0.30 alpha:1.0];
+}
+
+//Sets variable values to monochrome scheme
+-(void)setMonochromeColor {
+    bgColor = [UIColor lightGrayColor];
+    textColor = [UIColor darkGrayColor];
+    buttonColor = [UIColor whiteColor];
+    textViewColor = bgColor;
+    labelColor = [UIColor blackColor];
+    
+    /*
+    self.bgColor = [UIColor lightGrayColor];
+    self.textColor = [UIColor darkGrayColor];
+    self.buttonColor = [UIColor whiteColor];
+    self.labelColor = [UIColor blackColor];
+     */
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
