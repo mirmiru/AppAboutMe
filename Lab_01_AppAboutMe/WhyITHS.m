b49f9e56d6add87496bbfe805f78d13dc49e5035
Milja <mmcvirtanen@gmail.com>
2018-01-19 18:43:42 +0100

    Images and WhyITHS class.

---
+++ Lab_01_AppAboutMe/WhyITHS.m
@@ -0,0 +1,37 @@
+//
+//  WhyITHS.m
+//  Lab_01_AppAboutMe
+//
+//  Created by Milja Virtanen on 2018-01-18.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import "WhyITHS.h"
+
+@interface WhyITHS ()
+
+@end
+
+@implementation WhyITHS
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
